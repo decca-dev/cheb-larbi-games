6b740603b8f57795a208d6fe69a41844a78924d6
decca-dev <medcherbani@gmail.com>
2022-05-29 00:05:46 +0100

    404 and 401

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,10 +3,10 @@ import Layout from "../components/Layout";
 import { useMetaData } from "../lib/hooks/useMetaData";
 import Image from "next/image";
 
-const fourOone: NextPage = () => {
+const fourOfour: NextPage = () => {
   return (
     <>
-      {useMetaData("401 | Unauthenticated", "401 | Unauthenticated", "/401")}
+      {useMetaData("404 | Not found", "404 | Not found", "/404")}
       <Layout>
         <div className="flex flex-col items-center bg-gray-200 rounded-xl shadow-xl pb-8">
           <div>
@@ -17,15 +17,15 @@ const fourOone: NextPage = () => {
               height="250"
             />
           </div>
-          <h1 className="pt-10 font-bold text-3xl">401 | Unauthenticated</h1>
+          <h1 className="pt-10 font-bold text-3xl">404 | Not found</h1>
           <h2 className="pt-3 pb-5 font-bold text-xl">
-            You need to be logged in to view this route
+            There are no resources on this page
           </h2>
           <button
             className="text-white border-0 py-2 px-5 focus:outline-none rounded text-base mt-4 bg-green-600 md:mt-0"
-            onClick={() => (window.location.href = "/auth/login")}
+            onClick={() => (window.location.href = "/")}
           >
-            Login
+            Go home
           </button>
         </div>
       </Layout>
@@ -33,4 +33,4 @@ const fourOone: NextPage = () => {
   );
 };
 
-export default fourOone;
+export default fourOfour;
