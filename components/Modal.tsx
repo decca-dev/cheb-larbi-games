0d13609a7ef955991f63f7ca67a91611f6074836
decca-dev <medcherbani@gmail.com>
2022-05-29 00:05:26 +0100

    modal changes

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,27 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useRef, Dispatch, SetStateAction } from "react";
+import { useOutsideAlerter } from "../lib/hooks/useOutsideAlerter";
 
-const Modal: FC<{ children: ReactNode; show: boolean }> = ({
-  children,
-  show,
-}) => {
+const Modal: FC<{
+  children: ReactNode;
+  show: boolean;
+  setShow: Dispatch<SetStateAction<boolean>>;
+}> = ({ children, show, setShow }) => {
+  const modalRef = useRef(null);
+  useOutsideAlerter(modalRef, () => setShow(false));
   if (!show) {
     return null;
   }
   return (
-    <div className="fixed left-0 right-0 top-0 bottom-0 bg-gray-600 flex items-center justify-center">
-      {children}
+    <div
+      className="fixed left-0 right-0 top-0 bottom-0 flex items-center justify-center"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+    >
+      <div
+        className="container w-auto h-auto bg-gray-200 rounded-xl shadow-xl pt-8 pb-8 text-black"
+        ref={modalRef}
+      >
+        {children}
+      </div>
     </div>
   );
 };
